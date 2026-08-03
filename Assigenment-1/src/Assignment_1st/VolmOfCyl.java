e10e33f04e9b267de071a714f9bcb5188f017487
adityas8817-star <adityas8817@gmail.com>
2026-08-03 12:20:57 +0530

    [Aditya]commit

---
+++ Assigenment-1/src/Assignment_1st/VolmOfCyl.java
@@ -0,0 +1,19 @@
+package Assignment_1st;
+import java.util.Scanner;
+public class VolmOfCyl {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the radius: ");
+        double radius = sc.nextDouble();
+
+        System.out.print("Enter the height: ");
+        double height = sc.nextDouble();
+
+        double volume = Math.PI * radius * radius * height;
+
+        System.out.println("Volume of the cylinder = " + volume);
+
+        sc.close();
+    }
+}
---
+++ Assigenment-1/src/Assignment_1st/WelcomeToBridgeLabz.java
@@ -0,0 +1,8 @@
+package Assignment_1st;
+
+public class WelcomeToBridgeLabz {
+        public static void main(String[] args) {
+            System.out.println("Welcome to Bridgelabz!");
+        }
+    }
+
